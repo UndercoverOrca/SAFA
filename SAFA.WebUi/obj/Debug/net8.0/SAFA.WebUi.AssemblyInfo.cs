7206f5b1233cfa5bda2721ca58585959a1fc8075
Tim van der Nol <tim.vandernol@vicompany.nl>
2024-08-19 14:12:30 +0200

    [Frontend] - Add layout

--- SAFA.WebUi/obj/Debug/net8.0/SAFA.WebUi.AssemblyInfo.cs
+++ SAFA.WebUi/obj/Debug/net8.0/SAFA.WebUi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SAFA.WebUi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3ac2bf413787a697258964ae4d093a98d97feb8d")]
 [assembly: System.Reflection.AssemblyProductAttribute("SAFA.WebUi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SAFA.WebUi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
